38ee77746f07f27f5d82e2b9b0f336a7f36462f7
Dmitry Shmelev <avikez@gmail.com>
2017-03-31 16:05:31 +0300

    No LCD

--- arduino-esp8266-mhz-19-serial.ino
+++ arduino-esp8266-mhz-19-serial.ino
@@ -4,10 +4,8 @@
 #include <WiFiClient.h>
 #include <Wire.h>
 #include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson
-#include <LiquidCrystal_I2C.h> // https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
 #include "WiFiUtils.h"
 #include "WiFiCreds.h"
-#include "LcdPrint.h"
 #include "dataServer.h"
 
 #define INTERVAL 5000
@@ -24,7 +22,6 @@ long previousMillis = 0;
 int errorCount = 0;
 WiFiClient client;
 WiFiUtils wifiUtils;
-lcdPrint lcd(0x3F, 16, 2); // display address and size
 DHT dht(DHT_PIN, DHT_VERSION);//define temperature and humidity sensor
 SoftwareSerial co2Serial(MH_Z19_RX, MH_Z19_TX); // define MH-Z19
 
@@ -73,15 +70,15 @@ bool sendData(JsonObject& root)
     client.println("Host: " + String(DATA_SERVER));
     client.println("Connection: close");
     client.println("User-Agent: Arduino/1.0");
-    client.println("Content-Type: application/x-www-form-urlencoded;");
+    client.println("Content-Type: application/json;");
 
     client.print("Content-Length: ");
     String data;
     root.printTo(data);
 
-    //Serial.println("data to send:");
-    //Serial.println(data);
-    data = "data=" + data;
+    Serial.println("data to send:");
+    Serial.println(data);
+    // data = "data=" + data;
     client.println(data.length());
     client.println();
     client.println(data);
@@ -124,10 +121,6 @@ void setup() {
       delay(1000);
   }
 
-
-  lcd.begin();
-  lcd.backlight();
-  lcd.printLine(1, "Connecting...");
   // attempt to connect to Wifi network:
   unsigned int attempt = 0;
   while ( WiFi.status() != WL_CONNECTED) {
@@ -136,7 +129,6 @@ void setup() {
     if (attempt >= 65535)
       attempt = 0;
     attempt++;
-    lcd.printLine(2, "Attempt " + String(attempt));
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network:
@@ -152,14 +144,10 @@ void setup() {
   wifiUtils.printWifiData();
 
   Serial.println("Waiting for sensors to init");
-  lcd.printLine(1, "Heating...");
-  lcd.printLine(2, "");
   while (millis() - previousMillis < 10000)
     delay(1000);
   Serial.println("Setup finished");
   Serial.println("");
-  lcd.printLine(1, "Heating...");
-  lcd.noBacklight();
 }
 
 void loop()
@@ -174,9 +162,6 @@ void loop()
   if (errorCount > MAX_DATA_ERRORS)
   {
     Serial.println("Too many errors, resetting");
-    lcd.printLine(1, "Too many errors.");
-    lcd.printLine(2, "Trying to reset.");
-    lcd.backlight();
     delay(2000);
     resetFunc();
   }
@@ -188,17 +173,7 @@ void loop()
   if (ppm < 100 || ppm > 6000)
   {
     Serial.println("PPM not valid");
-    lcd.printParam(2, "PPM err");
     dataError = true;
-    lcd.noBacklight();
-  }
-  else
-  {
-    lcd.printParam(2, "PPM=" + String(ppm));
-    if (ppm >= 1000)
-      lcd.backlight();
-    else
-      lcd.noBacklight();
   }
   int mem = ESP.getFreeHeap();
   Serial.println("  Free RAM: " + String(mem));
@@ -214,24 +189,17 @@ void loop()
   if (t < 5 || t > 80 )
   {
     Serial.println("Temperature not valid");
-    lcd.printParam(3, "T err");
     dataError = true;
   }
-  else
-    lcd.printParam(3, "T=" + String(t) + (char)223);
 
   if (h > 100 || h == 0)
   {
     Serial.println("Humidity not valid");
-    lcd.printParam(1, "H err");
     dataError = true;
   }
-  else
-    lcd.printParam(1, "H=" + String(h) + "%");
 
   if (dataError)
   {
-    lcd.printParam(4, "WiFi skp");
     Serial.println("Skipping loop");
     errorCount++;
     return;
@@ -254,13 +222,7 @@ void loop()
     wifiUtils.setGoogleDNS();
   wifiUtils.printCurrentNet();
 
-  lcd.printParam(4, "WiFi *");
   bool sentOk = sendData(root);
-  Serial.println("Request sent");
-  if (sentOk)
-    lcd.printParam(4, "WiFi ok!");
-  else
-    lcd.printParam(4, "WiFi err");
   Serial.println("loop finished");
   Serial.println("");
 }
